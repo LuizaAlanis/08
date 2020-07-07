75cff4a9a97be1facc5ffdf1160c8ac4ff9736c8
LuizaAlanis <luiza.ferreira10@etec.sp.gov.br>
2020-07-06 22:47:02 -0300

    Inclusão do projeto.

--- src/cardapiolanchonete/CardapioLanchonete.java
+++ src/cardapiolanchonete/CardapioLanchonete.java
@@ -5,15 +5,69 @@ public class CardapioLanchonete
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("informe um valor: ");
-        int n = sc.nextInt();
-        System.out.println("soma: "+ Soma(n));
-    }
-    
-    public static int Soma (int n)
-    {
-        int res = 0;
-        if(n == 1) return 1;
-        else return (n - 1) + (n);
+
+        int qt;
+        double pc;
+        String cd;
+        
+        System.out.println(" código    opção               preço");
+        System.out.println("(C)        Cachorro Quente     R$2");
+        System.out.println("(R)        Refrigerante        R$2,5");
+        System.out.println("(S)        Sobremesa           R$1,5");
+        
+        System.out.print("Digite o Codigo do Alimento: ");
+        cd = sc.nextLine();
+        
+        System.out.print("informe quantas unidades: ");
+        qt = sc.nextInt();
+        
+        int i = 1;
+        double pcn = 0;
+        
+        while(i!=0)
+        {
+            if("C".equals(cd))
+            {
+                pc = 2 * qt;
+                System.out.println(qt+ " Cachorro(s) Quente(s) custa R$"+pc);
+                pcn += pc;
+            }
+
+            if("R".equals(cd))
+            {
+                pc = 2.5*qt;
+                System.out.println(qt+ " Refrigerante(s) custa R$"+pc);
+                pcn += pc;
+            }
+
+            if("S".equals(cd))
+            {
+                pc = 1.5*qt;
+                System.out.println(qt+ " Sobremesa(s) custa R$"+pc);
+                pcn += pc;
+            }
+            else 
+            {
+                System.out.println("Informe o código em letra MAIUSCULA!");
+            }
+
+            System.out.print("Sair? Digite X: ");
+            String ss = sc.next();
+            
+            if("X".equals(ss))
+            {
+                System.out.println("Então no total fica: R$"+ pcn);
+                i = 0;
+            }
+            else 
+            {
+                System.out.print("Digite o Codigo do Alimento: ");
+                cd = sc.next();
+                System.out.print("informe quantas unidades: ");
+                qt = sc.nextInt();
+                i = 1;
+            }
+            System.out.println();
+        }
     }
 }
